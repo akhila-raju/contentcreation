bde556fc085e8890133006edf9efdcb861be2b30
Akhila Raju <akhilaraju@berkeley.edu>
2014-10-08 00:21:06 -0700

    fix errors

--- jnd.py
+++ jnd.py
@@ -1,12 +1,12 @@
-import sys, time, datetime
+import os, sys, time, datetime
 
 def newParticipant(number):
-	print('Creating new folder for Participant' + number)
 	if os.path.exists('Participant' + number):
+		print('Creating new folder for Participant' + number)
    		os.makedirs('Participant' + number) # make folder for participant
 
 		print('Creating text files') # create files in participant folder
-		experiments = [potentiometer]
+		experiments = ['potentiometer']
 		for item in experiments: #record values for each file
 			file = open(item + '.csv', 'w')
 			runExperiment(item)
@@ -28,3 +28,5 @@ def runExperiment(fileName):
 	timeElapsed = done - start
 	fileName.write('End: ' + datetime.datetime.now().strftime("%d/%m/%Y %H:%M"))
 	fileName.write('Time: ' + timeElapsed + ' s')
+
+newParticipant('three')
