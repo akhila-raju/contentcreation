5a3bd2f8fdce4e7f84e899a85364be88670b2b79
Akhila Raju <akhilaraju@berkeley.edu>
2014-10-08 00:46:54 -0700

    successfully creates participant subfolders + csv files

--- jnd.py
+++ jnd.py
@@ -2,19 +2,20 @@ import os, sys, time, datetime
 
 def newParticipant(number):
 	if not os.path.exists('Participant' + number):
-		print('Creating new folder for Participant' + number)
+		print('Creating new folder for Participant' + number + '...')
    		os.makedirs('Participant' + number) # make folder for participant
 
-		print('Creating text files') # create files in participant folder
-		experiments = ['potentiometer']
+		print('Creating text files...') # create files in participant folder
+		experiments = ['Potentiometer']
 		for item in experiments: #record values for each file
-			file = open(os.path.join('Participant' + number, item + '.csv'), 'w')
+			fileName = open(os.path.join('Participant' + number, item + '.csv'), 'w')
+			runExperiment(fileName, item)
 	else:
 		print('Participant folder already exists.')
 
 
-def runExperiment(fileName):
-	print('Running ' + fileName + ' Experiment... ')
+def runExperiment(fileName, experimentName):
+	print('Running ' + experimentName + ' Experiment... ')
 	fileName.write('Start: ' + datetime.datetime.now().strftime("%d/%m/%Y %H:%M"))
 	start = time.time()
 	#if (fileName == 'potentiometer'):
@@ -26,6 +27,6 @@ def runExperiment(fileName):
 	done = time.time()
 	timeElapsed = done - start
 	fileName.write('End: ' + datetime.datetime.now().strftime("%d/%m/%Y %H:%M"))
-	fileName.write('Time: ' + timeElapsed + ' s')
+	fileName.write('Time: ' + str(timeElapsed) + ' s')
 
-newParticipant('three')
+newParticipant(raw_input('Enter Participant Number: '))
