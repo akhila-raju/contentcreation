318e842ad718acb3a4f06ff389eba575b332d245
Akhila Raju <akhilaraju@berkeley.edu>
2014-10-23 23:15:03 -0700

    add json, change file structure after experiment design

--- jnd.py
+++ jnd.py
@@ -1,63 +1,50 @@
-import os, sys, time, datetime, json, requests
+import os, sys, time, datetime, json
 from pylab import *
 from scipy.optimize import curve_fit
 
+#Create specified actuator folder and specified participant folder
 def newActuator(actuatorName, participantNumber):
 	if not os.path.exists(actuatorName):
 		print('Creating new folder for ' + actuatorName + '...')
 		os.makedirs(actuatorName) # make folder for actuator
 	print('Creating participant folder...') # create participant subfolder within actuator folder, CSV
-	experiments = [["light", "dark"]]
+#	experiments = [["light", "dark"]]
 	participant = 'Participant'
 	participant += str(participantNumber)
 	participantFolder = actuatorName + '/' + participant + '/'
 	os.makedirs(participantFolder)
-	fileName = open(os.path.join(participantFolder, participantNumber + '.csv'), 'w')
-	runExperiment(fileName, actuatorName, experiments[item], item)
+	runExperiment(actuatorName, item)
 
-def runExperiment(fileName, actuatorName, experimentName, participantNumber):
-	print('Running ' + actuatorName + ' ' + experimentName + ' Experiment for Participant ' + participantNumber + ' ...')
-	fileName.write('Start: ' + datetime.datetime.now().strftime("%d/%m/%Y %H:%M"))
-	start = time.time()
-	#if (fileName == 'potentiometer'):
-		# everytime the button is pressed, record the values
-		# control the number of times the button is pressed
-		#for each (button press):
-		#	fileName.write(value)
-		# how do you know when the experiment has ended?
-	done = time.time()
-	timeElapsed = done - start
-	# data =
-	# fileName.write('\n End: ' + datetime.datetime.now().strftime("%d/%m/%Y %H:%M"))
-	# fileName.write('\n Time: ' + str(timeElapsed) + ' s')
-#remove csv
+#Create JSON for specified actuator and specifie participant
+def runExperiment(actuatorName, participantNumber):
+	print('Running ' + actuatorName + ' Experiment for Participant ' + participantNumber + ' ...')
+	data = {}
+	data['actuator'] = actuatorName
+	data['participant'] = participantNumber
+	data['startTime'] = str(datetime.datetime.now().strftime("%m/%d/%Y %H:%M"))
+	data['x-values'] = 
+	data['y-values'] = #function
+	#buttonpresses
+	json_data = json.dumps(data)
 
-# {StartTime: ,  , EndTime: }
+#post json to URL
+#def postJson(json):
+	#url = "http://localhost:8080" #talk to Jasper about where to post, send Json text
+	# r = requests.post(url, data=jsonText), headers=headers)
 
-def postJson(path):
-	f = open(path, 'r')
-	arr = []
-	headers = []
-	for header in f.readline().split(','):
-		headers.append(header)
 
-	for line in f.readlines():
-		lineItems = {}
-		for i,item in enumerate(line.split(',')):
-			lineItems[headers[i]] = item
-		arr.append(lineitems)
+def regressionFunc(x, a, b, c, d):
+    return a*np.exp(-c*(x-b))+d
 
-	f.close()
+# def actuatorVis():
+# 	#visualization that plots data points of all participants
+# 	#plot 
+# 	# perform exponential regression
+# 	# visualize by exporting json to D3
 
-	jsonText = json.dumps(arr)
+# def participantVis():
+# 	#visualize for a particular participant
 
-	url = "http://localhost:8080" #talk to Jasper about where to post, send Json text
-	r = requests.post(url, data=jsonText), headers=headers)
-
-def visualize():
-	#go into each participant folder and create a visualization for an experiment of an actuator
-	# perform exponential regression
-	# visualize by exporting json to D3
 
 actuatorName = raw_input('Enter Actuator Name: ')
 participantNumber = raw_input('Participant Number: ')
@@ -68,5 +55,3 @@ newActuator(actuatorName, participantNumber)
 # -ls outputs all of the actuator names that we have on file
 # -r "actuator name" "id"
 # - participant
-# fit on power law curve; 2 values = a, k
-# export it to JSON or CSV
